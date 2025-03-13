00aa35bdac634a66fbcbed9f0cdd61453d395b43
um4own <um4own@gmail.com>
2025-03-13 14:02:20 +0900

    공백추가(의미없음)

--- api.py
+++ api.py
@@ -8,11 +8,11 @@ import json
 from datetime import datetime
 import pytz
 import pandas as pd
+
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
-
 client_id = os.getenv("client_id")
 clientSecret = os.getenv("clientSecret")
 
