5c412c5eeaa61af697c6a476af5a644e398147b4
um4own <um4own@gmail.com>
2025-03-18 17:06:02 +0900

    tailwind installed

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"], // React 컴포넌트에서 Tailwind 클래스를 인식할 수 있도록 설정
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
