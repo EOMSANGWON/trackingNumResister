0ba7ee1f5bd616dc7eb4432ced5cfa4a9c2278f7
EOMSANGWON <164110360+EOMSANGWON@users.noreply.github.com>
2025-04-01 17:08:43 +0900

    Delete tailwind.config.js

--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"], // React 컴포넌트에서 Tailwind 클래스를 인식할 수 있도록 설정
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
